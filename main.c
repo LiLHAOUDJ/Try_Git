029560f1f2a666244312bc2adafd0a3fcc511ef3
Ridouane <lilhaoudj@gmail.com>
2025-08-25 09:05:18 +0100

    second commit

--- main.c
+++ main.c
@@ -4,5 +4,11 @@ int main(int argc, char const *argv[])
 {
 	printf("Paix et Salut de Dieu sur Notre Bien Aimé !!!\n");
 	printf("");
+	signed char name; int old;
+	printf("What is your name : ");
+	scanf("%s", &name);
+	printf("Haw old have you : ");
+	scanf("%d", &old);
+	printf("Welcom Mr/M. %s you have %d old",&name, &old);
 	return 0;
 }
